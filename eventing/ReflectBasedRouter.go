dda04ed99d04ea5b06fdc79e462d4467807c3954
Pieter Joost van de Sande <pj@born2code.net>
2013-07-14 14:34:40 +0200

    Refactor ReflectBasedRouter

--- eventing/ReflectBasedRouter.go
+++ eventing/ReflectBasedRouter.go
@@ -7,35 +7,52 @@ import (
 
 var MethodHandlerPrefix = "Handle"
 
+type HandlersMap map[EventName]EventHandler
+
 // Routes events to methods of an struct by convention. There should be one
 // router per event source instance.
 //
 // The convention is: HandleXXX(e EventType)
 type ReflectBasedRouter struct {
-	handlers map[EventName]EventHandler
+	handlers HandlersMap
 	source   interface{}
 }
 
 func NewReflectBasedRouter(namer EventNamer, source interface{}) EventRouter {
 	sourceType := reflect.TypeOf(source)
-	handlers := make(map[EventName]EventHandler)
+	handlers := make(HandlersMap)
 
+	// Loop through all the methods of the source
 	for i := 0; i < sourceType.NumMethod(); i++ {
 		method := sourceType.Method(i)
 
-		// Handler method will have the following signature from an reflection
-		// point of view: HandleUserCreated(*domain.User, events.UserCreated)
-		if strings.HasPrefix(method.Name, MethodHandlerPrefix) && method.Type.NumIn() == 2 {
-			handler := func(e Event) {
-				method.Func.Call([]reflect.Value{reflect.ValueOf(source), reflect.ValueOf(e)})
+		// Only match methods that satisfy prefix
+		if strings.HasPrefix(method.Name, MethodHandlerPrefix) {
+			// Warning: Handling methods are defined in code by:
+			//   func (source *MySource) HandleMyEvent(e MyEvent).
+			// When getting the type of this methods by reflection the signature
+			// is as following:
+			//   func HandleMyEvent(source *MySource, e MyEvent).
+			if method.Type.NumIn() == 2 {
+				handler := createEventHandler(source, method)
+				eventName := namer.GetEventNameFromType(method.Type.In(1))
+				handlers[eventName] = handler
 			}
-			eventName := namer.GetEventNameFromType(method.Type.In(1))
-			handlers[eventName] = handler
 		}
 	}
 
 	return &ReflectBasedRouter{
 		handlers: handlers,
+	}
+}
+
+func createEventHandler(source interface{}, method reflect.Method) EventHandler {
+	return func(event Event) {
+		sourceValue := reflect.ValueOf(source)
+		eventValue := reflect.ValueOf(event)
+
+		// Call actual event handling method.
+		method.Func.Call([]reflect.Value{sourceValue, eventValue})
 	}
 }
 
