e212968598a82e37831f96ea9b5fc02113facc8a
Pieter Joost van de Sande <pj@born2code.net>
2013-07-27 11:31:27 +0200

    Adds test for EventSourceId

---
+++ sourcing/EventSourceId_test.go
@@ -0,0 +1,47 @@
+package sourcing
+
+import (
+	"encoding/json"
+	. "launchpad.net/gocheck"
+)
+
+// The state for the test suite
+type EventSourceIdTestSuite struct {
+}
+
+// Setup the test suite
+var _ = Suite(&EventSourceIdTestSuite{})
+
+// Make sure we can turn an EventSourceId into a JSON value
+func (s *EventSourceIdTestSuite) TestMarshallJSON(c *C) {
+	id := NewEventSourceId()
+
+	t := &struct {
+		Id    EventSourceId  `json:"id"`
+		IdPtr *EventSourceId `json:"idPtr"`
+	}{
+		Id:    id,
+		IdPtr: &id,
+	}
+
+	b, err := json.Marshal(t)
+	c.Assert(err, IsNil)
+
+	c.Assert(string(b), Equals, "{\"id\":\""+t.Id.String()+"\",\"idPtr\":\""+t.IdPtr.String()+"\"}")
+}
+
+// Make sure we can turn an JSON value into an EventSourceId
+func (s *EventSourceIdTestSuite) TestUnMarshallJSON(c *C) {
+	t := &struct {
+		Id    EventSourceId  `json:"id"`
+		IdPtr *EventSourceId `json:"idPtr"`
+	}{}
+
+	id := NewEventSourceId()
+	data := []byte("{\"id\":\"" + id.String() + "\",\"idPtr\":\"" + id.String() + "\"}")
+	err := json.Unmarshal(data, &t)
+	c.Assert(err, IsNil)
+
+	c.Assert(t.Id, Equals, id)
+	c.Assert(*t.IdPtr, Equals, id)
+}
