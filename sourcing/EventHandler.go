5aa1394688106f12c8aa1fe6f14f1f9713d90690
Pieter Joost van de Sande <pj@born2code.net>
2013-07-27 13:24:55 +0200

    Remove unused type EventHandler

--- sourcing/EventHandler.go
+++
@@ -1,5 +0,0 @@
-package sourcing
-
-// An methods that can handle an event. Most event handlers will receive
-// only specific types and they should do the type assertion themself.
-type EventHandler func(e Event)
