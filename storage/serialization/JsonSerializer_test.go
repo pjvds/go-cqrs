749393eaf5336327615fef5253537fc8864d8fa6
Pieter Joost van de Sande <pj@born2code.net>
2013-08-08 21:58:42 +0200

    Finalize tests for JsonSerializer

--- storage/serialization/JsonSerializer_test.go
+++ storage/serialization/JsonSerializer_test.go
@@ -57,10 +57,14 @@ func (suite *JsonSerializerTestSuite) TestSerialize(c *C) {
 }
 
 func (suite *JsonSerializerTestSuite) TestDerialize(c *C) {
-	json := "{\"eventId\":\"25f7fdb6-5ef9-47b0-55a1-b9160ce37730\",\"name\":\"github.com/pjvds/go-cqrs/storage/serialization/FooEvent\",\"sequence\":0,\"timestamp\":\"2011-02-01T12:32:40-05:00\",\"payload\":{\"Foo\":\"\",\"Bar\":0}}"
+	json := "{\"eventId\":\"25f7fdb6-5ef9-47b0-55a1-b9160ce37730\",\"name\":\"github.com/pjvds/go-cqrs/storage/serialization/FooEvent\",\"sequence\":0,\"timestamp\":\"2011-02-01T12:32:40-05:00\",\"payload\":{\"Foo\":\"hello world\",\"Bar\":42}}"
 	serializer := suite.serializer
 
 	event, err := serializer.Deserialize(suite.fooEventName, []byte(json))
 	c.Assert(err, IsNil)
 	c.Assert(event, NotNil)
+	c.Assert(event.Data, FitsTypeOf, new(FooEvent))
+
+	c.Assert(event.Data.(*FooEvent).Foo, Equals, "hello world")
+	c.Assert(event.Data.(*FooEvent).Bar, Equals, 42)
 }
