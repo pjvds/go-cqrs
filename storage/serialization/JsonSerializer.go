a83219a6f8b2f6eacbc6ac514f0fa76215702c61
Pieter Joost van de Sande <pj@born2code.net>
2013-08-08 20:43:04 +0200

    Fix JsonSerializer

--- storage/serialization/JsonSerializer.go
+++ storage/serialization/JsonSerializer.go
@@ -2,22 +2,60 @@ package serialization
 
 import (
 	"encoding/json"
-	"github.com/pjvds/go-cqrs/sourcing"
+	"errors"
+	"fmt"
+	"github.com/pjvds/go-cqrs/storage"
+	"reflect"
+	"time"
 )
 
+type JsonDocument map[string]interface{}
+
+// Holds the meta information for an event with the data
+// object as a raw message so that it can be deserialized in two stages.
+type jsonEvent struct {
+	EventId   storage.EventId       `json:"eventId"`   // The id of the event itself
+	Name      storage.EventName     `json:"name"`      // The event name, this value is also used for type identification (maps name to Go type)
+	Sequence  storage.EventSequence `json:"sequence"`  // The sequence of the event which starts at zero.
+	Timestamp time.Time             `json:"timestamp"` // The point in time when this event happened.
+	Data      json.RawMessage       `json:"payload"`   // The data of the event.
+}
+
 type JsonSerializer struct {
+	types *EventTypeRegister
 }
 
-func NewJsonSerializer() *JsonSerializer {
-	return &JsonSerializer{}
+func NewJsonSerializer(types *EventTypeRegister) *JsonSerializer {
+	return &JsonSerializer{
+		types: types,
+	}
 }
 
-func (s *JsonSerializer) Serialize(e *sourcing.Event) ([]byte, error) {
+func (s *JsonSerializer) Serialize(e *storage.Event) ([]byte, error) {
 	data, err := json.Marshal(e)
 
 	return data, err
 }
 
-func (s *JsonSerializer) Deserialize(data []byte, e *sourcing.Event) error {
-	return json.Unmarshal(data, e)
+func (s *JsonSerializer) Deserialize(name storage.EventName, data []byte) (*storage.Event, error) {
+	raw := new(jsonEvent)
+
+	if err := json.Unmarshal(data, raw); err != nil {
+		return nil, err
+	}
+
+	e := new(storage.Event)
+	e.EventId = raw.EventId
+	e.Name = raw.Name
+	e.Sequence = raw.Sequence
+	e.Timestamp = raw.Timestamp
+
+	eventType, ok := s.types.Get(name)
+	if !ok {
+		return e, errors.New(fmt.Sprintf("No known type for %v, register it first", name.String()))
+	}
+	event := reflect.New(eventType)
+	e.Data = event.Interface()
+
+	return e, nil
 }
---
+++ storage/serialization/JsonSerializer_test.go
@@ -0,0 +1,66 @@
+package serialization
+
+import (
+	"github.com/pjvds/go-cqrs/storage"
+	. "launchpad.net/gocheck"
+	"reflect"
+	"time"
+)
+
+type FooEvent struct {
+	Foo string
+	Bar int
+}
+
+type JsonSerializerTestSuite struct {
+	fooEventType reflect.Type
+	fooEventName storage.EventName
+
+	serializer *JsonSerializer
+}
+
+// Setup the test suite
+var _ = Suite(&JsonSerializerTestSuite{})
+
+func (suite *JsonSerializerTestSuite) SetUpSuite(c *C) {
+	namer := storage.NewTypeEventNamer()
+
+	event := new(FooEvent)
+	eventType := reflect.TypeOf(event)
+	eventName := namer.GetEventNameFromType(eventType)
+
+	types := NewEventTypeRegister()
+	types.Register(eventName, eventType)
+
+	suite.fooEventName = eventName
+	suite.fooEventType = eventType
+	suite.serializer = NewJsonSerializer(types)
+}
+
+func (suite *JsonSerializerTestSuite) TestSerialize(c *C) {
+	expectedJson := "{\"eventId\":\"25f7fdb6-5ef9-47b0-55a1-b9160ce37730\",\"name\":\"github.com/pjvds/go-cqrs/storage/serialization/FooEvent\",\"sequence\":0,\"timestamp\":\"2011-02-01T12:32:40-05:00\",\"payload\":{\"Foo\":\"\",\"Bar\":0}}"
+
+	timestamp, _ := time.Parse(time.RFC3339, "2011-02-01T12:32:40-05:00")
+
+	eventId, err := storage.ParseEventId("\"25f7fdb6-5ef9-47b0-55a1-b9160ce37730\"")
+	c.Assert(err, IsNil)
+
+	event := new(FooEvent)
+
+	envelope := storage.NewEvent(*eventId, suite.fooEventName, storage.NewEventSequence(0), timestamp, event)
+	data, err := suite.serializer.Serialize(envelope)
+
+	c.Assert(err, IsNil)
+
+	actualJson := string(data)
+	c.Assert(actualJson, Equals, expectedJson)
+}
+
+func (suite *JsonSerializerTestSuite) TestDerialize(c *C) {
+	json := "{\"eventId\":\"25f7fdb6-5ef9-47b0-55a1-b9160ce37730\",\"name\":\"github.com/pjvds/go-cqrs/storage/serialization/FooEvent\",\"sequence\":0,\"timestamp\":\"2011-02-01T12:32:40-05:00\",\"payload\":{\"Foo\":\"\",\"Bar\":0}}"
+	serializer := suite.serializer
+
+	event, err := serializer.Deserialize(suite.fooEventName, []byte(json))
+	c.Assert(err, IsNil)
+	c.Assert(event, NotNil)
+}
