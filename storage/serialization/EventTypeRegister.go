9cc1c2868447860d94f1726bfa0fb7d5e92508d4
Pieter Joost van de Sande <pj@born2code.net>
2013-08-08 20:41:55 +0200

    Move EventTypeRegister to serialization

--- storage/serialization/EventTypeRegister.go
+++ storage/serialization/EventTypeRegister.go
@@ -1,6 +1,7 @@
-package storage
+package serialization
 
 import (
+	"github.com/pjvds/go-cqrs/storage"
 	"reflect"
 )
 
@@ -8,25 +9,25 @@ import (
 // All event type should be registered at bootstrap time so that an event store,
 // bus or other services can deserialize messages to concrete types.
 type EventTypeRegister struct {
-	types map[EventName]reflect.Type
+	types map[storage.EventName]reflect.Type
 }
 
 // Creates a new register with an empty map.
 func NewEventTypeRegister() *EventTypeRegister {
 	return &EventTypeRegister{
-		types: make(map[EventName]reflect.Type, 0),
+		types: make(map[storage.EventName]reflect.Type, 0),
 	}
 }
 
 // Registers an event type. An existing entry with the same name is overwritten
 // if it exists.
-func (register *EventTypeRegister) Register(n EventName, t reflect.Type) {
+func (register *EventTypeRegister) Register(n storage.EventName, t reflect.Type) {
 	register.types[n] = t
 }
 
 // Get the static type from an event name. It results `true` for `ok` if
 // the type was found; otherwise, `false`.
-func (register *EventTypeRegister) Get(n EventName) (reflect.Type, bool) {
+func (register *EventTypeRegister) Get(n storage.EventName) (reflect.Type, bool) {
 	t, ok := register.types[n]
 	return t, ok
 }
