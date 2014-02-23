69169a98ae4cdfea1b2a1a140ae57b72ea37d12f
Dominik Mayer <dominik.mayer@gmail.com>
2014-02-23 20:42:01 +0100

    Finished implementing Mongo support.

--- storage/EventStreamChange.go
+++ storage/EventStreamChange.go
@@ -6,3 +6,19 @@ type EventStreamChange struct {
 	To       int64
 	Events   []*Event
 }
+
+type EventStreamChangePersist struct {
+	StreamId string
+	From     int64
+	To       int64
+	Events   []*Event
+}
+
+func (e *EventStreamChange) GetPersistableObject() *EventStreamChangePersist {
+	return &EventStreamChangePersist{
+		StreamId:  e.StreamId.String(),
+		From: e.From,
+		To: e.To,
+		Events: e.Events,
+	}
+}
