ba167e144e5dc5a47e342b404198e6d5a24eef45
Dominik Mayer <dominik.mayer@gmail.com>
2014-02-25 00:31:13 +0100

    Converted first gocheck tests to GoConvey

--- tests/Sourcing_test.go
+++ tests/Sourcing_test.go
@@ -4,87 +4,63 @@ import (
 	"github.com/dominikmayer/go-cqrs/sourcing"
 	"github.com/dominikmayer/go-cqrs/tests/domain"
 	"github.com/dominikmayer/go-cqrs/tests/events"
-	. "launchpad.net/gocheck"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) {
-	InitLogging()
-	TestingT(t)
+func TestStateChangesAreRepresentedByEvents(t *testing.T) {
+
+	Convey("Given we create a new user 'pjvds'", t, func() {
+		user := domain.NewUser("pjvds")
+
+		Convey("Then the username should be 'pjvds'", func() {
+			So(user.Username, ShouldEqual, "pjvds")
+		})
+
+		Convey("And the creation should be captured by an event", func() {
+			So(len(user.Events()), ShouldEqual, 1)
+		})
+
+		Convey("When we change the username to 'wwwouter'", func() {
+			user.ChangeUsername("wwwouter")
+
+			Convey("Then this should also be captured by an event", func() {
+				So(len(user.Events()), ShouldEqual, 2)
+			})
+		})
+	})
 }
 
-// The state for the test suite
-type AppTestSuite struct {
-}
+func TestDomainObjectCanBeBuildFromHistory(t *testing.T) {
 
-// Setup the test suite
-var _ = Suite(&AppTestSuite{})
+	Convey("Given the user and the name changes from the last test", t, func() {
 
-func (s *AppTestSuite) TestStateChangesAreRepresentedByEvents(c *C) {
-	// Create a new domain object
-	user := domain.NewUser("pjvds")
-	c.Assert(user.Username, Equals, "pjvds")
+		// The id of our event source that we will rebuild from history.
+		sourceId, _ := sourcing.ParseEventSourceId("0791d279-664d-458e-bf60-567ade140832")
 
-	// We created a new user, this should be
-	// captured by an event.
-	c.Assert(len(user.Events()), Equals, 1)
+		// The full history for the User domain object
+		history := []sourcing.Event{
+			// It was first created
+			events.UserCreated{
+				Username: "pjvds",
+			},
+			// Then the username was changed
+			events.UsernameChanged{
+				OldUsername: "pjvds",
+				NewUsername: "wwwouter",
+			},
+		}
 
-	// Change the username of the user
-	user.ChangeUsername("wwwouter")
-	c.Assert(user.Username, Equals, "wwwouter")
+		Convey("When we create a new User domain object from the event history", func() {
+			user := domain.NewUserFromHistory(sourceId, history)
 
-	// We changed the username, this should be
-	// captured by an event.
-	c.Assert(len(user.Events()), Equals, 2)
-}
+			Convey("Then the username should not be 'pjvds'", func() {
+				So(user.Username, ShouldNotEqual, "pjvds")
+			})
 
-func (s *AppTestSuite) TestDomainObjectCanBeBuildFromHistory(c *C) {
-	// The id of our event source that we will rebuild from history.
-	sourceId, _ := sourcing.ParseEventSourceId("0791d279-664d-458e-bf60-567ade140832")
-
-	// The full history for the User domain object
-	history := []sourcing.Event{
-		// It was first created
-		events.UserCreated{
-			Username: "pjvds",
-		},
-		// Then the username was changed
-		events.UsernameChanged{
-			OldUsername: "pjvds",
-			NewUsername: "wwwouter",
-		},
-	}
-
-	// Create a new User domain object from history
-	user := domain.NewUserFromHistory(sourceId, history)
-
-	// It should not have the initial state.
-	c.Assert(user.Username, Not(Equals), "pjvds")
-
-	// It should have the latest state.
-	c.Assert(user.Username, Equals, "wwwouter")
-}
-
-func (s *AppTestSuite) BenchmarkRebuildUserFromHistory(c *C) {
-	// The full history for the User domain object
-	sourceId, _ := sourcing.ParseEventSourceId("0791d279-664d-458e-bf60-567ade140832")
-
-	// The full history for the User domain object
-	history := []sourcing.Event{
-		// It was first created
-		events.UserCreated{
-			Username: "pjvds",
-		},
-		// Then the username was changed
-		events.UsernameChanged{
-			OldUsername: "pjvds",
-			NewUsername: "wwwouter",
-		},
-	}
-
-	for i := 0; i < c.N; i++ {
-		// Create a new User domain object from history
-		domain.NewUserFromHistory(sourceId, history)
-	}
+			Convey("But it should be 'wwwouter'", func() {
+				So(user.Username, ShouldEqual, "wwwouter")
+			})
+		})
+	})
 }
